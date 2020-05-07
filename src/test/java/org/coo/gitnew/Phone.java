e4747d25859ec006a0fd36a5d6ebc5ebea391b62
Bhavasruthy <bhavasruthyd@gmail.com>
2020-05-07 12:36:34 +0530

    Samsung added

--- src/test/java/org/coo/gitnew/Phone.java
+++ src/test/java/org/coo/gitnew/Phone.java
@@ -2,7 +2,8 @@ package org.coo.gitnew;
 
 public class Phone {
 	public static void main(String[] args) {
-System.out.println("Phone");    
+System.out.println("Phone"); 
+System.out.println("Samsung");
 	}
 	
 }
